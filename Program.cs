e1023d95e42ca572fa5fff5902cb55d58cf974bf
ManwathaW <Manwathawanga112@gmail.com>
2024-09-27 10:52:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BeerAssignment
+{
+
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            //  instance of a BeerSong class 
+            BeerSong myBeerSong = new BeerSong();
+
+            //calling the Sing() method on myBeerSong instance.
+            myBeerSong.Sing();
+        }
+    }
+}
+
---
+++ Sing.cs
@@ -0,0 +1,20 @@
+﻿namespace BeerAssignment
+{
+    public class BeerSong
+    {
+        public void Sing()
+        {
+            // Loop through the bottles of beer
+            for (int bottles = 99; bottles >= 1; bottles--)
+            {
+                // Determine whether to use "bottle" or "bottles" based on the current count
+                string bottleText = bottles == 1 ? "bottle" : "bottles";
+                string nextBottleText = (bottles - 1) == 1 ? "bottle" : "bottles";
+
+                // Print the current verse
+                Console.WriteLine($"{bottles} {bottleText} of beer on the wall, {bottles} {bottleText} of beer. Take one down and pass it around\n");
+
+            }
+        }
+    }
+}
